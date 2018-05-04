5e089dcac165d189f9d5c59ad5e93fdd233687a6
Jannik Hellenkamp <jannik.hellenkamp@rwth-aachen.de>
2018-05-04 22:10:47 +0200

    changed stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/posts/:id/notify', to: 'posts#notify'
+  get '/posts/:id/time', to: 'posts#time'
+  
+  root :to => "posts#index"
 end
